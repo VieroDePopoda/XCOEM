7cfcadc9445329651b602315faf46bfcfdb8e951
VieroDePopoda <tr0p6661@gmail.com>
2020-06-07 23:44:25 +0300

    Все представления теперь в Main, вместо миллиона html. Роутинги.

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace XCOEM
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Category}/{action=Main}");
             });
         }
     }
